c309c82daca5b4da2b9d6e7a4a2db26d00ad7c35
Clinkz-1 <44914835+Clinkz-1@users.noreply.github.com>
2019-08-14 11:26:49 +0800

    Update aiohttp_test.py

--- buff/aiohttp_test.py
+++ buff/aiohttp_test.py
@@ -13,8 +13,12 @@ async def main(url):
         print(html)
 
 
-loop = asyncio.get_event_loop()
+def aiohttp_run(url_list):
+    loop = asyncio.get_event_loop()
 
-tasks = [main(url) for url in url_list]
-loop.run_until_complete(asyncio.wait(tasks))
-loop.close()
+    tasks = [main(url) for url in url_list]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+
+if __name__ == '__main__':
+    aiohttp_run(url_list)
