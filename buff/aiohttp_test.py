3be475c68c3b473b80f9b0962e6a69ad6f26ad68
Clinkz-1 <44914835+Clinkz-1@users.noreply.github.com>
2019-08-14 12:03:28 +0800

    Update aiohttp_test.py

--- buff/aiohttp_test.py
+++ buff/aiohttp_test.py
@@ -3,22 +3,28 @@ import asyncio
 
 url_list = ['https://buff.163.com/market/?game=dota2#tab=selling&page_num={}'.format(i) for i in range(20)]
 
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+async def main(pool,url_list):  # 启动
+    sem = asyncio.Semaphore(pool)
+    async with aiohttp.ClientSession() as session:  # 给所有的请求，创建同一个session
+        tasks = []
+        for url in url_list:
+            tasks.append(control_sem(sem,url,session))
+        await asyncio.wait(tasks)
 
-async def main(url):
-    async with aiohttp.ClientSession() as session:
-        html = await fetch(session, url)
-        print(html)
 
+async def control_sem(sem, url, session):  # 限制信号量
+    async with sem:
+        await fetch(url, session)
+
+
+async def fetch(url, session):  # 开启异步请求
+    async with session.get(url,headers=headers) as resp:
+        html = await resp.text()
+        print(url)
 
 def aiohttp_run(url_list):
     loop = asyncio.get_event_loop()
-
-    tasks = [main(url) for url in url_list]
-    loop.run_until_complete(asyncio.wait(tasks))
-    loop.close()
+    loop.run_until_complete(main(2,url_list))
 
 if __name__ == '__main__':
     aiohttp_run(url_list)
